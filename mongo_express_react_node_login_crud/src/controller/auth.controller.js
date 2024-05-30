d46a90164801c96a8e6f41806e7ba2c0c7be20e9
Aquiles <santtialbiazul25@gmail.com>
2024-05-30 14:58:41 -0300

    encriptacion de contraseña y guardado de objetos id_user_email

--- mongo_express_react_node_login_crud/src/controller/auth.controller.js
+++ mongo_express_react_node_login_crud/src/controller/auth.controller.js
@@ -2,19 +2,29 @@
 //export const login = (req, res) => res.send('login');
 
 import User from '../models/user.model.js'
+import bcrypt from 'bcryptjs'
 
 export const register = async (req, res) => {
     const {username, email, password } = req.body
     
     try {
+        
+        const passwordHash = await bcrypt.hash(password, 10) //encripta la contraseña
+        
         const newUser = new User({
             username,
             email,
-            password
+            password: passwordHash
         })
         //console.log(newUser)
-        await newUser.save(); //guarda los datos en la base de datos de manera asincrona
-        res.send('registrando')
+        const userSaved = await newUser.save(); //guarda los datos en la base de datos de manera asincrona
+        //res.send('registrando')
+        //res.json(newUser)
+        res.json({
+            id: userSaved._id,
+            username: userSaved.username,
+            email: userSaved.email,
+        })
     } catch (error) {
         console.log(error)
     }
